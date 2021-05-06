b6eb8d71cb00652bf55495427cb665d901321dbe
Arthur Bruno Moreira Barbosa <arthur.barbosa@ITBL000531D.itbam.org.br>
2021-05-06 14:30:33 -0400

    eureka-server e confi com api loja e fornecedor

--- loja/src/main/java/br/com/abcode/loja/service/CompraService.java
+++ loja/src/main/java/br/com/abcode/loja/service/CompraService.java
@@ -2,6 +2,7 @@ package br.com.abcode.loja.service;
 
 import br.com.abcode.loja.controller.dto.CompraDTO;
 import br.com.abcode.loja.controller.dto.InfoFornecedorDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,13 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class CompraService {
 
+    @Autowired
+    private RestTemplate client;
+
+
     public void realizaCompra(CompraDTO compra) {
-        RestTemplate client = new RestTemplate();
         ResponseEntity<InfoFornecedorDTO> exchange =
-                client.exchange("http://localhost:8081/info/" + compra.getEndereco().getEstado()
+                client.exchange("http://fornecedor/info/" + compra.getEndereco().getEstado()
                         , HttpMethod.GET, null, InfoFornecedorDTO.class);
 
         System.out.println(exchange.getBody().getEndereco());
